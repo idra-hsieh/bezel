fe6a1abec41af19e47623043569f6bc86a9e9232
Idra Hsieh <idra.hsieh@gmail.com>
2025-12-31 14:06:44 +0800

    build: add core dependencies for Mapbox, Riverpod, Isar, and Geolocator

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import geolocator_apple
+import isar_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
 }
